29990c117cd23b3745942241f18dd69a4be16c34
kishtarn555 <hec_montemoon@hotmail.com>
2025-01-24 14:35:41 -0600

    [Feature] Add all old code from the website

---
+++ src/colors.ts
@@ -0,0 +1,62 @@
+export type RendererColors = {
+    beeperBackgroundColor: string,
+    beeperColor: string,
+    disabled: string,
+    exportCellBackground: string,
+    karelColor: string,
+    gridBackgroundColor: string,
+    errorGridBackgroundColor: string,
+    gridBorderColor: string,
+    errorGridBorderColor: string,
+    gutterBackgroundColor: string,
+    gutterColor: string,
+    wallColor: string,
+    waffleColor: string,
+    gutterSelectionColor: string,
+    gutterSelectionBackgroundColor: string
+}
+
+export const DefaultRendererColors: RendererColors = {
+    disabled: '#4f4f4f',
+    exportCellBackground: '#f5f7a8',
+    karelColor: '#3E6AC1',
+    gridBackgroundColor: '#ffffff',
+    errorGridBackgroundColor: "#f5d5d5",
+    gridBorderColor: '#c4c4c4',
+    errorGridBorderColor: '#a8838f',
+    gutterBackgroundColor: '#e6e6e6',
+    gutterColor: "#444444",
+    beeperBackgroundColor: "#0ADB23",    
+    beeperColor: "#000000",
+    wallColor:"#000000",    
+    waffleColor: "#0d6dfd",
+    gutterSelectionBackgroundColor: "#86afd5",
+    gutterSelectionColor: "#000000",
+}
+
+export function isWRStyle(obj: any): obj is RendererColors {
+    if (!obj || typeof obj !== 'object') return false;
+
+    const requiredKeys = [
+        'disabled',
+        'exportCellBackground',
+        'karelColor',
+        'gridBackgroundColor',
+        'errorGridBackgroundColor',
+        'gridBorderColor',
+        'errorGridBorderColor',
+        'gutterBackgroundColor',
+        'gutterColor',
+        'beeperBackgroundColor',
+        'beeperColor',
+        'wallColor'
+    ];
+
+    for (const key of requiredKeys) {
+        if (!(key in obj) || typeof obj[key] !== 'string') {
+            return false;
+        }
+    }
+
+    return true;
+}
---
+++ src/data.ts
@@ -0,0 +1,13 @@
+export type CellPair = {
+    r: number,
+    c: number,
+}
+
+export type CellRegion = {
+    r: number
+    c: number
+    rows: number
+    cols: number
+    dr: number
+    dc: number
+}
